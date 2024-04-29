0f7818c8665d7652f133a8c18007c6743ee1ab55
Wang Zheng <wangzheng422@gmail.com>
2024-04-29 17:49:22 +0800

    span

--- spring-native/src/main/java/io/opentelemetry/example/graal/Controller.java
+++ spring-native/src/main/java/io/opentelemetry/example/graal/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
@@ -39,8 +40,9 @@ public class Controller {
     // and return the response
     // You can use libraries like HttpClient or OkHttp to make the request
     // Here's an example using HttpClient:
-    Span span = tracer.spanBuilder("makeHttpRequest").startSpan();
+    Span span = tracer.spanBuilder("makeHttpRequest").setSpanKind(SpanKind.CLIENT).startSpan();
     
+    HttpResponse<String> response = null;
     try (Scope ignored = span.makeCurrent()) {
 
       HttpClient client = HttpClient.newHttpClient();
@@ -48,20 +50,20 @@ public class Controller {
           .uri(URI.create(url))
           .build();
       try {
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        // span.end();
-    
-        return response.body();
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
       } catch (IOException | InterruptedException e) {
         e.printStackTrace();
-
-        // span.end();
-    
-        return null;
+        response = null;
       }
     } finally {
       span.end();
+    }
+
+    if (response!=null) {
+      return response.body()
+    }
+    else {
+      return null;
     }
   }
 
