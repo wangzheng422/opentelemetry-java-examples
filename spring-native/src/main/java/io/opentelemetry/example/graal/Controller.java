6cda15e8b0e9774605944501de6db963a8ab8435
Wang Zheng <wangzheng422@gmail.com>
2024-04-29 11:02:28 +0800

    app config

--- spring-native/src/main/java/io/opentelemetry/example/graal/Controller.java
+++ spring-native/src/main/java/io/opentelemetry/example/graal/Controller.java
@@ -31,23 +31,18 @@ public class Controller {
     // and return the response
     // You can use libraries like HttpClient or OkHttp to make the request
     // Here's an example using HttpClient:
-    Tracer tracer = GlobalOpenTelemetry.getTracer("sample-app");
-    Span span = tracer.spanBuilder("makeRequest").startSpan();
-    try (Scope ignored = span.makeCurrent()) {
-      HttpClient client = HttpClient.newHttpClient();
-      HttpRequest request = HttpRequest.newBuilder()
-          .uri(URI.create(url))
-          .build();
-      try {
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
-      } catch (IOException | InterruptedException e) {
-        e.printStackTrace();
-        return null;
-      }
-    } finally {
-      span.end();
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(url))
+        .build();
+    try {
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      return response.body();
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+      return null;
     }
+
   }
 
 }
